7cdff9a8156f763fc6c0cd5d6ec5b1a787cca0e1
bgyyo622-Boduen <bgyyo622@photographybyai.net>
2026-05-19 19:34:08 +0800

    Update status-manager.ts

--- src/github/status-manager.ts
+++ src/github/status-manager.ts
@@ -1,12 +1,8 @@
 import * as core from "@actions/core";
 
 export class StatusManager {
-  /**
-   * 強制執行架構阻斷 (Architecture Block Enforcement)
-   * 確保違反環境獨立語意 (EISV) 的程式碼無法進入主分支
-   */
   public static enforceBlock(violationCount: number): void {
-    const message = `[AICA_POLICY_ENFORCEMENT] Detected ${violationCount} instance(s) of implicit coupling. The system has lost extensional equivalence. PR merge is blocked.`;
+    const message = `[AICA_POLICY_ENFORCEMENT] Detected ${violationCount} instance(s) of implicit coupling. Extensional equivalence broken. PR merge blocked.`;
     core.setFailed(message);
   }
 
